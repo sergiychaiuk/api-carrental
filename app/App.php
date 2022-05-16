297a43e8f955fc3ea6f4b8f4381afb444f4951e2
serhiichaiuk <chayck7@gmail.com>
2022-05-16 21:19:13 +0300

    created methods for connect and disconnect in Database class; created init method and added him in index file

--- app/App.php
+++ app/App.php
@@ -2,7 +2,13 @@
 
 namespace App;
 
+use Core\Database;
+
 class App
 {
-
+    static public function init()
+    {
+        Database::connect();
+        Database::disconnect();
+    }
 }
--- core/Database.php
+++ core/Database.php
@@ -2,7 +2,41 @@
 
 namespace Core;
 
+use mysqli;
+
 class Database
 {
+    static protected $database;
 
+    static public function connect()
+    {
+        $config = require_once 'config/database.php';
+
+        $connection =  new mysqli(
+            $config['hostname'],
+            $config['username'],
+            $config['password'],
+            $config['database']
+        );
+
+        self::confirmConnect($connection);
+        self::$database = $connection;
+    }
+
+    static protected function confirmConnect($connection)
+    {
+        if ($connection->connect_errno) {
+            $msg = 'Database connection failed: ';
+            $msg .= $connection->connect_error;
+            $msg .= ' (' . $connection->connect_errno . ')';
+            exit($msg);
+        }
+    }
+
+    static public function disconnect()
+    {
+        if (isset(self::$database)) {
+            self::$database->close();
+        }
+    }
 }
