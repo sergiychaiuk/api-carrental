fd61c312e9620c98cb0eab0196b601301b3ad797
serhiichaiuk <chayck7@gmail.com>
2022-05-18 19:15:21 +0300

    created methods for get one or all record with db

--- core/Database.php
+++ core/Database.php
@@ -39,4 +39,54 @@ class Database
             self::$database->close();
         }
     }
+
+    static public function sql($sql)
+    {
+        $result = self::$database->query($sql);
+
+        if (!$result) {
+            exit("Database query failed.");
+        }
+
+        $object_array = [];
+
+        while ($record = $result->fetch_assoc()) {
+            $object_array[] = static::instantiate($record);
+        }
+
+        $result->free();
+
+        return $object_array;
+    }
+
+    static public function all() {
+        $sql = "select * from " . static::$table_name;
+        return static::sql($sql);
+    }
+
+    static public function find($id) {
+        $sql = "select * from " . static::$table_name . " ";
+        $sql .= "where id = '" . self::$database->escape_string($id) . "'";
+
+        $obj_array = static::sql($sql);
+
+        if (!empty($obj_array)) {
+            return array_shift($obj_array)  ;
+        } else {
+            return false;
+        }
+    }
+
+    static protected function instantiate($record): Database
+    {
+        $object = new static;
+
+        foreach ($record as $property => $value) {
+            if (property_exists($object, $property)) {
+                $object->$property = $value;
+            }
+        }
+
+        return $object;
+    }
 }
